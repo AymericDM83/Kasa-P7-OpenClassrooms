749401c853c06c0c36446423d97fb3dceb09998d
Chill83 <aymericmaggio@gmail.com>
2022-09-30 17:56:52 +0200

    About page Banner + responsive ok

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Banner = () => {
+  const path = useLocation().pathname;
+  const location = path.split("/")[1];
+
   return (
-    <div className="banner_container">
+    <div className={"banner_container" + location}>
       <p>
         Chez vous, <span>partout et ailleurs</span>
       </p>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 const About = () => {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Banner />
+    </div>
+  );
 };
 
 export default About;
