0a3908ddb50d0ff74cbe81acaa11227cc69a3456
Chill83 <aymericmaggio@gmail.com>
2022-10-03 18:04:53 +0200

    Error404 page + responsive ok

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import Header from "../components/Header";
 
 const Error404 = () => {
   return (
     <div>
       <Header />
+      <div className="error_block">
+        <div className="error_container">
+          <p className="error_number">404</p>
+          <p className="error_message">
+            Oups ! La page que vous demandez n'existe pas.
+          </p>
+        </div>
+        <NavLink to="/">
+          <div className="error_link">
+            <p>Retournez sur la page d'accueil</p>
+          </div>
+        </NavLink>
+      </div>
     </div>
   );
 };
